24ebf8848f81c20c02093c68e05ceb314e0ccdef
Mustafa Fayez <mustafa@integreight.com>
2013-12-02 09:52:06 +0200

    Initialize Repo

---
+++ ATmega Firmata/ATmega Firmata.c
@@ -0,0 +1,17 @@
+/*
+ * ATmega_Firmata.c
+ *
+ * Created: 2/12/2013 9:43:21 AM
+ *  Author: iMustafa
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
