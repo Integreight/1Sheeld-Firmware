857be5fa2c8cd9ff5cc89a5d5940d9d414b87629
Ahmed Fahmy <fahmy@integreight.com>
2013-12-10 22:18:21 +0200

    add system files

---
+++ ATmega Firmata/CommonMacros.h
@@ -0,0 +1,21 @@
+﻿/*
+ * IncFile1.h
+ *
+ * Created: 16/02/2013 11:08:06 م
+ *  Author: HP
+ * Write any macro that is not related to specific file
+ *
+ */ 
+
+
+
+#ifndef COMMON_MACROS_H_
+#define COMMON_MACROS_H_
+
+#define CLR_BIT(var, bit) var &= (~(1<<bit))
+#define SET_BIT(var, bit) var |= (1<<bit)
+#define TOG_BIT(var, bit) var ^= (1<<bit)
+
+
+
+#endif /* INCFILE1_H_ */
---
+++ ATmega Firmata/sys.h
@@ -0,0 +1,18 @@
+﻿/*
+ * sys.h
+ *
+ * Created: 19/03/2013 09:06:38 ص
+ *  Author: HP
+ */ 
+
+
+#ifndef SYS_H_
+#define SYS_H_
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+
+
+
+
+#endif /* SYS_H_ */
