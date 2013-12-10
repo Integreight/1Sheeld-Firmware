ee9f0a9cfcbc606f95a29c99130fab5c13598c29
Ahmed Fahmy <fahmy@integreight.com>
2013-12-10 22:14:26 +0200

    added gpio component closes #4

--- ATmega Firmata/ATmega Firmata.c
+++ ATmega Firmata/ATmega Firmata.c
@@ -5,13 +5,38 @@
  *  Author: iMustafa
  */ 
 
-
 #include <avr/io.h>
+#include "gpio.h"
+#include "uart.h"
+#include "CommonMacros.h"
+#include <util/delay.h>
+
 
 int main(void)
 {
+	uint8  i=0; 
+	t_SetPortCfg cfg;
+	UartInit(0,9600);
+	
+	cfg.Portdir=OUTPUT;
+	cfg.initValue=0x00;
+	cfg.pID=C;
+	GPIO_Cfg(&cfg);
+	
+	t_stPort* stport;
+	stport=(t_stPort *)cfg.pID;
+	
+	
     while(1)
     {
-        
+        //UartTx0('S');
+		UartTx0(UartRx0());
+		//GPIO_setPort(UartRx0(),cfg.pID);
+		//GPIO_setPort(i++,cfg.pID);
+		_delay_ms(100);
+		
+		
+		TOG_BIT((stport->portOutReg),5);
+		//TODO:: Please write your application code 
     }
 }
---
+++ ATmega Firmata/gpio.h
@@ -0,0 +1,62 @@
+﻿/*
+ * gpio.h
+ *
+ * Created: 27/09/2013 02:17:32 م
+ *  Author: HP
+ */ 
+
+
+#ifndef GPIO_H_
+#define GPIO_H_
+#include"sys.h"
+
+
+typedef enum {
+	
+	INPUT=0x00,
+	OUTPUT=0xFF
+}t_ePortDirection;
+
+typedef struct
+{
+	uint8 portInReg;//pin
+	uint8 portDirReg;//ddrc
+	uint8 portOutReg;//portc
+	
+}t_stPort;
+
+//Registers addresses in the memory 
+typedef enum {
+	
+	A=0x39,
+	B=0x36,
+	C=0x33,
+	D=0x30
+}t_ePortID;
+
+typedef enum {
+	
+	PinIn=0,
+	PinOUT=1
+}t_ePinDirection;
+
+
+
+typedef struct
+{
+	t_ePortDirection Portdir;	
+	uint8 initValue;
+	t_ePortID pID;
+	t_ePinDirection pindir;
+	
+	
+	
+}t_SetPortCfg;
+
+void GPIO_Cfg(t_SetPortCfg* cfg);
+void GPIO_setPort(uint8 data,t_ePortID pid);
+uint8 GPIO_getPort(t_ePortID pid);
+uint8 GPIO_getPin(t_ePortID pid,uint8 pinNum);
+void GPIO_setPin(uint8 data,t_ePortID pid,uint8 pinNum);
+
+#endif /* GPIO_H_ */
