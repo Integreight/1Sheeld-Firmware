d2afb567a2ee27a786cea30b573bf3e2c1177e76
Mustafa Fayez <mustafa@integreight.com>
2013-12-24 21:51:18 +0200

    complete the mapping file closes #5

--- ATmega Firmata/Mapping.h
+++ ATmega Firmata/Mapping.h
@@ -1,8 +1,14 @@
-#ifndef Mapping_h
-#define Mapping_h
+#ifndef mapping_h
+#define mapping_h
 
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <avr/pgmspace.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #include "CommonMacros.h"
+
 
 #define TOTAL_PINS              35 
 #define digitalPinHasPWM(p)         ((p) == 3 || (p) == 5 || (p) == 6 ||  (p) == 9||  (p) == 10||  (p) == 11)
@@ -14,6 +20,20 @@
 #define NUM_ANALOG_INPUTS           0
 #define analogInputToDigitalPin(p)  (-1)
 
+// values
+#define HIGH 0x1
+#define LOW  0x0
+
+#define INPUT 0x0
+#define OUTPUT 0x1
+#define INPUT_PULLUP 0x2
+
+#define true 0x1
+#define false 0x0
+
+
+#define NOT_A_PIN 0
+#define NOT_A_PORT 0
 // SPI pins 
 static const uint8_t SS   = 10;
 static const uint8_t MOSI = 28;
@@ -220,5 +240,13 @@ static inline unsigned char writePort(byte port, byte value, byte bitmask)
 	if ((bitmask & 0x80)) digitalWrite(PIN_TO_DIGITAL(pin+7), (value & 0x80));
 }
 
+
+#define digitalPinToPort(P) ( pgm_read_byte( digital_pin_to_port_PGM + (P) ) )
+#define digitalPinToBitMask(P) ( pgm_read_byte( digital_pin_to_bit_mask_PGM + (P) ) )
+#define digitalPinToTimer(P) ( pgm_read_byte( digital_pin_to_timer_PGM + (P) ) )
+#define analogInPinToBit(P) (P)
+#define portOutputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_output_PGM + (P))) )
+#define portInputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_input_PGM + (P))) )
+#define portModeRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_mode_PGM + (P))) )
 
 #endif // Mapping_h
