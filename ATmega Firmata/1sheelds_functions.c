0b63c2326ee0b6e64ef9adefd36354725ae319de
Ahmed Fahmy <fahmy@integreight.com>
2013-12-24 21:55:16 +0200

    Creating 1sheelds files and Creating the functions prototypes

---
+++ ATmega Firmata/1sheelds_functions.c
@@ -0,0 +1,6 @@
+﻿/*
+ * _1sheelds_functions.c
+ *
+ * Created: 24/12/2013 08:53:17 م
+ *  Author: HP
+ */ 
---
+++ ATmega Firmata/1sheelds_functions.h
@@ -0,0 +1,24 @@
+﻿/*
+ * _1sheelds_functions.h
+ *
+ * Created: 24/12/2013 08:51:42 م
+ *  Author: HP
+ */ 
+
+
+#ifndef 1SHEELDS_FUNCTIONS_H_
+#define 1SHEELDS_FUNCTIONS_H_
+
+#include "sys.h"
+
+uint8  digitalRead(uint8);
+void   digitalWrite(uint8, uint8);
+void   pinMode(uint8 , uint8);
+uint16 analogRead(uint8);
+void   analogWrite(uint8_t, uint16);
+
+
+
+
+
+#endif /* 1SHEELDS_FUNCTIONS_H_ */
