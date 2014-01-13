7fa4bb22388bb213580db5fba1fed436d5f5b347
Ahmed Fahmy <fahmy@integreight.com>
2014-01-13 13:46:27 +0200

    create  pwm MCAL layer closes #1

---
+++ ATmega Firmata/pwm.c
@@ -0,0 +1,84 @@
+﻿/*
+ * pwm.c
+ *
+ * Created: 01/01/2014 09:47:40 م
+ *  Author: HP
+ */ 
+#include <avr/io.h>
+#include "sys.h"
+#include "pwm.h"
+
+void pwm_Setup(uint8 timer)
+{
+	switch(timer)
+	{
+		case TIMER_00 :
+			TCCR0|=(1<<WGM00)|(1<<WGM01)|(1<<CS00)|(1<<COM01);
+			break;
+		
+		case TIMER_1A:
+			TCCR1A|=(1<<WGM10)|(1<<COM1A1);
+			TCCR1B|=(1<<WGM12)|(1<<CS10);	
+			break;
+		
+		case TIMER_1B:
+			TCCR1A|=(1<<WGM10)|(1<<COM1B1);
+			TCCR1B|=(1<<WGM12)|(1<<CS10);	
+			break;
+		
+		case TIMER_20:
+			TCCR2|=(1<<WGM20)|(1<<WGM21)|(1<<CS20)|(1<<COM01);
+			break;
+		
+		case TIMER_3A:
+			TCCR3A|=(1<<WGM30)|(1<<COM3A1);
+			TCCR3B|=(1<<WGM32)|(1<<CS30);
+			break;
+		
+		case TIMER_3B:
+			TCCR3A|=(1<<WGM30)|(1<<COM3B1);
+			TCCR3B|=(1<<WGM32)|(1<<CS30);
+			break;	
+		
+		default:
+			break;	
+				 	
+	}
+}
+
+void pwm_SetDutyCycle(uint8 dutyCycle, uint8 timer_No )
+{
+	
+	switch(timer_No)
+	{
+		case TIMER_00 :
+			OCR0 = dutyCycle;
+			break;
+		
+		case TIMER_1A:
+			OCR1A = dutyCycle;
+			break;
+		
+		case TIMER_1B:
+			OCR1B = dutyCycle;
+			break;
+		
+		case TIMER_20:
+			OCR2 = dutyCycle;
+			break;
+		
+		case TIMER_3A:
+			OCR3A = dutyCycle;
+			break;
+			
+		case TIMER_3B:
+			OCR3B = dutyCycle;	
+			break;
+		
+		default:
+			break;
+		
+	}
+}
+
+
---
+++ ATmega Firmata/pwm.h
@@ -0,0 +1,29 @@
+﻿/*
+ * pwm.h
+ *
+ * Created: 01/01/2014 09:47:59 م
+ *  Author: HP
+ */ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+
+#define TIMER_00		0
+#define TIMER_1A		1
+#define TIMER_1B		2
+#define TIMER_20		3
+#define TIMER_3A		4
+#define TIMER_3B		5
+
+
+
+void pwm_Setup(uint8);
+void pwm_SetDutyCycle(uint8, uint8);
+
+
+
+
+
+#endif /* PWM_H_ */
