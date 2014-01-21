fcedb515adc8930ce08658595c3582d9b700dd23
Mustafa Fayez <mustafa@integreight.com>
2014-01-21 20:01:57 +0200

    create firmata162 files in project closes #19

---
+++ ATmega Firmata/firmata162.c
@@ -0,0 +1,7 @@
+/*
+ * firmata162.c
+ *
+ * Created: 21/1/2014 7:59:39 PM
+ *  Author: iMustafa
+ */ 
+#include "firmata162.h"
---
+++ ATmega Firmata/firmata162.h
@@ -0,0 +1,16 @@
+/*
+ * firmata162.h
+ *
+ * Created: 21/1/2014 7:59:18 PM
+ *  Author: iMustafa
+ */ 
+
+
+#ifndef FIRMATA162_H_
+#define FIRMATA162_H_
+
+
+
+
+
+#endif /* FIRMATA162_H_ */
