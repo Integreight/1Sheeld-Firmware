4ed74a5df3b3ae3a8e11e585ff8ed7f914374314
Islam Mustafa <islam@integreight.com>
2014-03-04 11:11:55 +0200

    Added a utility function to know how much ram left

--- ATmega Firmata/ATmega Firmata.cpp
+++ ATmega Firmata/ATmega Firmata.cpp
@@ -16,7 +16,11 @@
 #include "firmata.h"
 #include <util/delay.h>
 
-
+int freeRam () {
+	extern int __heap_start, *__brkval;
+	int v;
+	return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+}
 
 int main(void)
 {
