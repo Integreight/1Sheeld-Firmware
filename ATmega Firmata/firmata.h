bb6213e49178fd4f0e78fd10fb67d1b7cf4b21cd
Mustafa Fayez <mustafa@integreight.com>
2014-04-06 14:16:23 +0200

    Fixed an old conflict

--- ATmega Firmata/firmata.h
+++ ATmega Firmata/firmata.h
@@ -65,12 +65,9 @@
 #define FIRMATA_MUTE            0x64  // turn off firmata
 #define FIRMATA_VERSION         0X63 // our firmata version
 #define IS_ALIVE                0x62
-<<<<<<< HEAD
 #define RESET_BLUETOOTH         0x61          
-=======
 #define RESET_MICRO             0x60
          
->>>>>>> rxtxfahmy
 // pin modes
 #define ANALOG                  0x02 // analog pin in analogInput mode
 #define PWM                     0x03 // digital pin in PWM output mode
